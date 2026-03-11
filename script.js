6081ec13c7919aef17fff89cfe00b7178902577d
Aryan <bhattiaryan777@gmail.com>
2026-03-11 13:58:04 +0530

    js file added , form functionality done , styling done

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', function(e) {
+document.querySelectorAll('nav a').forEach(anchor => {
+
+anchor.addEventListener("click", function(e) {
+
+const href = this.getAttribute("href");
+
+if(href.startsWith("#")){
+
 e.preventDefault();
 
-document.querySelector(this.getAttribute('href')).scrollIntoView({
-behavior: 'smooth'
+const target = document.querySelector(href);
+
+if(target){
+target.scrollIntoView({
+behavior: "smooth"
 });
+}
+
+}
+
 });
+
 });
